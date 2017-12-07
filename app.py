7984e385483fb0cf41f0955c882afa38417443ba
carmelo12341 <ertsheiker@gmail.com>
2017-12-07 14:21:57 -0500

    Some more changes - Updated cuz of change in images.py - Some naming changes - Updated way of getting information from the image uploaded Damn, I suck at making commits

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify, escape
 from flask_cors import CORS
 from utils.ipfs import IPFSUtils
-from utils.images import create_thumbnail
+from utils.images import ImagesUtils
 
 
 class API:
@@ -10,7 +10,8 @@ class API:
     """
     app = Flask(__name__)
     CORS(app)
-    utils = IPFSUtils()
+    ipfs_utils = IPFSUtils()
+    img_utils = ImagesUtils()
 
     def __init__(self):
         # Routes
@@ -37,33 +38,38 @@ class API:
         """
         # Thread information from request
         title = escape(request.json["thread_title"])
-        body = escape(request.json["thread_content"])
+        content = escape(request.json["thread_content"])
         post_id = request.json["post_id"]
-        image = request.json["thread_image_hashes"]
-        response_id = self.utils.make_thread(title, body, image, post_id)
+        image_hashes = request.json["thread_image_hashes"]
+        image_info = request.json["thread_image_info"]
+        response_id = self.ipfs_utils.make_thread(title, content, image_hashes, image_info, post_id)
         return response_id
 
     def upload_image(self):
         image_file = request.files["file"]
         post_id = request.form["post_id"]
         # Original image data
-        image_extension = image_file.filename.split(".")[-1]
-        image_basename = ".".join(image_file.filename.split(".")[0:-1])
-        image_filename = image_basename + "." + image_extension
-        image_ipfs_hash = self.utils.upload_image(image_file.read(), image_filename, post_id)
+        image_filename = image_file.filename
+        image_extension = image_filename.split(".")[-1]
+        image_format = image_file.mimetype.split("/")[-1]
+        image_basename = image_filename[:-(len(image_extension) + 1)]  # Plus the dot (.)
+        image_ipfs_hash = self.ipfs_utils.upload_image(image_file.read(), image_filename, post_id)
         # Thumbnail data
-        thumbnail_file = create_thumbnail(image_file)
-        thumbnail_filename = image_basename + "-thumbnail.jpeg"
-        thumbnail_ipfs_hash = self.utils.upload_image(thumbnail_file, thumbnail_filename, post_id)
-        # thumbnail_ipfs_hash = "test"
-        response = {
-            "original": image_ipfs_hash,
-            "thumbnail": thumbnail_ipfs_hash
-        }
+        thumbnail_file = self.img_utils.create_thumbnail(image_file, image_format)
+        thumbnail_filename = image_basename + "-thumbnail.jpg"
+        thumbnail_ipfs_hash = self.ipfs_utils.upload_image(thumbnail_file, thumbnail_filename, post_id)
+        image_information = self.img_utils.get_image_information(image_file, image_format, image_filename)
+        response = [
+            image_information,
+            {
+                "original": image_ipfs_hash,
+                "thumbnail": thumbnail_ipfs_hash
+            }
+        ]
         return jsonify(response)
 
     def get_threads(self):
-        threads_json = self.utils.get_threads()
+        threads_json = self.ipfs_utils.get_threads()
         return jsonify(threads_json)
 
 
